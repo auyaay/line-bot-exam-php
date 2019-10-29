13de9c2dba2b1b76014bdc566d177289ba6da219
auyaay <wutipong_eam@hotmail.com>
2019-10-29 15:29:48 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -4,7 +4,7 @@
 $access_token = 'qBoxjDO5Wk04JI+WRd3YrjkBE5znWb4aWRmoSFArYnXtljFdNDLz7REpcAvMQjyHYaNdCogLZAQNG+yLJO5yNxs4oQxqulZpV2zL9OrCxTY+ReMY9sS38dLGjMPZztegT/EHvs9hbBs4jhQsVpBb6QdB04t89/1O/w1cDnyilFU=
 ';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Ue11448d3151ac1c134689864dbe94e49';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
