4b2bb4c82958d7338bb47ea07356f9afe1c2a0c2
auyaay <wutipong_eam@hotmail.com>
2019-11-01 11:40:30 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,8 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = "hi : ";
-			$text .= "$event['source']['userId'];
+			
+			$text = "$event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
